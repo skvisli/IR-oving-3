de1dec7b158bfe9f67e8037ae983600a9b3a8cd9
skvisli <skvisli@gmail.com>
2017-10-20 16:12:53 +0200

    Finished

--- test.py
+++ test.py
@@ -5,8 +5,6 @@ import os
 import string
 import logging
 
-#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
 
 def removeHeadAndFoot(txt):
     sections = txt.split("***")
@@ -69,6 +67,15 @@ def removeStopWords(dictionary):
     print("Stop words removed")
     return dictionary
 
+
+# Prints the tokens and their weights of the query
+def printQueryTokenAndWeights(vec_tfidf):
+    for vec in vec_tfidf:
+        for i in range(0, vec.__len__()):
+            print(dictionary.get(vec[i][0]), " :", vec[i][1])
+    print("")
+
+
 # Prints top 3 paragraph matches for a query
 def printTop3Paragraphs(sims):
     for i in range(0, 3):
@@ -76,6 +83,16 @@ def printTop3Paragraphs(sims):
         print(paragraphs[sims[i][0]], "\n")
 
 
+def printTop3Topics(vec_lsi):
+    top3Topics = sorted(vec_lsi[0], key=lambda score: -abs(score[1]))[:3]
+    for topic in top3Topics:
+        print("[topic ", topic[0], "]")
+        print(lsiModel.print_topic(int(topic[0])))
+
+
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+# BUILDING CORPUS
 txt = codecs.open("pg3300.txt", "r", "utf-8").read()
 txt = removeHeadAndFoot(txt)
 paragraphs = splitIntoParagraphs(txt)
@@ -95,26 +112,21 @@ vec_bow = [dictionary.doc2bow(token) for token in queryStemmedTokens]  # Creates
 #TF-IDF
 tfidfModel = gensim.models.TfidfModel(corpus)  # Initialise TF-ID model used for transformation.
                                                # Takes a BoW representation and transforms it to TF-ID weights
-corpus_tfidf = tfidfModel[corpus]
+corpus_tfidf = tfidfModel[corpus]  # Creates a TF-IDF corpus from the BoW corpus
 vec_tfidf = tfidfModel[vec_bow]  # Transforms the query from BoW -> TF-IDF
-for vec in vec_tfidf:
-    print(dictionary.get(vec[0][0]), " :", vec[0][1])
-    print(dictionary.get(vec[1][0]), " :", vec[1][1])
-    print(dictionary.get(vec[2][0]), " :", vec[2][1])
+printQueryTokenAndWeights(vec_tfidf)
 index = gensim.similarities.MatrixSimilarity(corpus_tfidf)  # Indexes the TF-IDF space
-sims = index[vec_tfidf]  # Calculates the similarities between the query and the corpus
-sims = sorted(enumerate(sims[0]), key=lambda item: -item[1])  # Sorts the simularites by decending order and adds the index
-printTop3Paragraphs(sims)
+sims_tfidf = index[vec_tfidf]  # Calculates the similarities between the query and the corpus
+sims_tfidf = sorted(enumerate(sims_tfidf[0]), key=lambda item: -item[1])  # Sorts the simularites by decending order and adds the index
+printTop3Paragraphs(sims_tfidf)
 
 # LSI
 lsiModel = gensim.models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)  # initialize an LSI transformation model
                                                                                      # from the TFIDF corpus
-corpus_lsi = lsiModel[corpus_tfidf]
-vec_lsi = lsiModel[vec_tfidf]
+corpus_lsi = lsiModel[corpus_tfidf]  # Creates a LSI corpus from the TFID corpus
+vec_lsi = lsiModel[vec_tfidf]  # Transform the query from TF-IDF -> LSI
+printTop3Topics(vec_lsi)
 index = gensim.similarities.MatrixSimilarity(corpus_lsi)  # Indexes the LSI space
-sims = index[vec_lsi]  # Calculates the similarities between the query and the corpus
-sims = sorted(enumerate(sims[0]), key=lambda item: -item[1])  # Sorts the simularites by decending order and adds the index
-printTop3Paragraphs(sims)
-
-print(sorted(vec_lsi, key=lambda kv: -abs(kv[1]))[:3])
-
+sims_lsi = index[vec_lsi]  # Calculates the similarities between the query and the corpus
+sims_lsi = sorted(enumerate(sims_lsi[0]), key=lambda item: -item[1])  # Sorts the simularites by decending order and adds the index
+printTop3Paragraphs(sims_lsi)
