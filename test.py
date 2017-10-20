18d3855ec8f0fc5e28b277b4bd1f8e419a2c4723
skvisli <skvisli@gmail.com>
2017-10-20 15:09:56 +0200

    More cleanup TODO: print top 3 LSI topics

--- test.py
+++ test.py
@@ -5,12 +5,14 @@ import os
 import string
 import logging
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
 
 def removeHeadAndFoot(txt):
     sections = txt.split("***")
     mainSection = sections[2]
     return mainSection
+
 
 def splitIntoParagraphs(txt):
     # Splits mainTxt into paragraphs
@@ -20,6 +22,7 @@ def splitIntoParagraphs(txt):
         if para:
             paragraphs2.append(para)
     return paragraphs2
+
 
 # Tokenizes paragraphs
 def tokenize(paragraphs):
@@ -32,6 +35,7 @@ def tokenize(paragraphs):
             tokens.append(para.split())
     print("Book tokenized")
     return tokens
+
 
 # Stem tokens
 def stem(tokens):
@@ -46,12 +50,14 @@ def stem(tokens):
     print("Finished stemming")
     return stemmedTokens
 
+
 # Creates dictionary
 def createDictionary(stemmedTokens):
     dictionary = gensim.corpora.Dictionary()
     dictionary.add_documents(stemmedTokens)
     print("Dictionary created")
     return dictionary
+
 
 # Remove stop words
 def removeStopWords(dictionary):
@@ -63,6 +69,12 @@ def removeStopWords(dictionary):
     print("Stop words removed")
     return dictionary
 
+# Prints top 3 paragraph matches for a query
+def printTop3Paragraphs(sims):
+    for i in range(0, 3):
+        print("[", sims[i][0], "]")
+        print(paragraphs[sims[i][0]], "\n")
+
 
 txt = codecs.open("pg3300.txt", "r", "utf-8").read()
 txt = removeHeadAndFoot(txt)
@@ -70,43 +82,39 @@ paragraphs = splitIntoParagraphs(txt)
 tokens = tokenize(paragraphs)
 stemmedTokens = stem(tokens)
 dictionary = removeStopWords(createDictionary(stemmedTokens))
-corpus = [dictionary.doc2bow(token) for token in stemmedTokens]  # Creates a corpus (represented as a Bag of Words)from the book that can be used to train models
+corpus = [dictionary.doc2bow(token) for token in stemmedTokens]  # Creates a corpus (represented as a Bag of Words)
+                                                                 # from the book that can be used to train models
 
 # QUERY
 query = "How taxes influence Economics?"
 queryTokens = tokenize([query])
 queryStemmedTokens = stem(queryTokens)
-vec_bow = [dictionary.doc2bow(token) for token in queryStemmedTokens]
+vec_bow = [dictionary.doc2bow(token) for token in queryStemmedTokens]  # Creates a vector space for the query
+
 
 #TF-IDF
-tfidfModel = gensim.models.TfidfModel(corpus)  # Initialise TF-ID model used for transformation. Takes a BoW representation and transforms it to TF-ID weights
+tfidfModel = gensim.models.TfidfModel(corpus)  # Initialise TF-ID model used for transformation.
+                                               # Takes a BoW representation and transforms it to TF-ID weights
 corpus_tfidf = tfidfModel[corpus]
-vec_tfidf = tfidfModel[vec_bow]
+vec_tfidf = tfidfModel[vec_bow]  # Transforms the query from BoW -> TF-IDF
 for vec in vec_tfidf:
     print(dictionary.get(vec[0][0]), " :", vec[0][1])
     print(dictionary.get(vec[1][0]), " :", vec[1][1])
     print(dictionary.get(vec[2][0]), " :", vec[2][1])
-index = gensim.similarities.MatrixSimilarity(tfidfModel[corpus])
-sims = index[vec_tfidf]
-sims = sorted(enumerate(sims[0]), key=lambda item: -item[1])
-print(sims[0])
-print(paragraphs[sims[0][0]])
-print(paragraphs[sims[1][0]])
-print(paragraphs[sims[2][0]])
+index = gensim.similarities.MatrixSimilarity(corpus_tfidf)  # Indexes the TF-IDF space
+sims = index[vec_tfidf]  # Calculates the similarities between the query and the corpus
+sims = sorted(enumerate(sims[0]), key=lambda item: -item[1])  # Sorts the simularites by decending order and adds the index
+printTop3Paragraphs(sims)
 
 # LSI
 lsiModel = gensim.models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=100)  # initialize an LSI transformation model
+                                                                                     # from the TFIDF corpus
 corpus_lsi = lsiModel[corpus_tfidf]
-lsiModel.print_topics(100)
-vec_lsi = lsiModel[vec_bow]
-index = gensim.similarities.MatrixSimilarity(lsiModel[corpus])  # transform corpus to LSI space and index it
-sims = index[vec_lsi]  # perform a similarity query against the corpus
-sims = sorted(enumerate(sims[0]), key=lambda item: -item[1])  # Sorts sim decending on the similarity scores
-"""print(sims[0])  # print sorted (document number, similarity score) 2-tuples
-paragraaa = corpus.__getitem__(sims[0][0])
-for para in paragraaa:
-    print(dictionary.__getitem__(para[0]))
-print(paragraphs[sims[0][0]])
-print(paragraphs[sims[1][0]])
-print(paragraphs[sims[2][0]])
-"""
+vec_lsi = lsiModel[vec_tfidf]
+index = gensim.similarities.MatrixSimilarity(corpus_lsi)  # Indexes the LSI space
+sims = index[vec_lsi]  # Calculates the similarities between the query and the corpus
+sims = sorted(enumerate(sims[0]), key=lambda item: -item[1])  # Sorts the simularites by decending order and adds the index
+printTop3Paragraphs(sims)
+
+print(sorted(vec_lsi, key=lambda kv: -abs(kv[1]))[:3])
+
